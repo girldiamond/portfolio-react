278ecc6a0f58f7f02c80016f41b20144dd872f8e
girldiamond <epic-arol87@hotmail.com>
2019-10-02 11:51:00 -0600

    Updates

---
+++ precache-manifest.1c2066b3d71a4df1c6934b381f74753e.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e42bf110d6d2dd55cfbaa42c81b907d",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "e34675436550b68cd350",
+    "url": "/portfolio-react/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "7a505360d94bba5196b2",
+    "url": "/portfolio-react/static/css/main.799117a9.chunk.css"
+  },
+  {
+    "revision": "e34675436550b68cd350",
+    "url": "/portfolio-react/static/js/2.75ca3987.chunk.js"
+  },
+  {
+    "revision": "7a505360d94bba5196b2",
+    "url": "/portfolio-react/static/js/main.6196b5d9.chunk.js"
+  },
+  {
+    "revision": "11528ddda4154ce1e32b",
+    "url": "/portfolio-react/static/js/runtime~main.72b3206a.js"
+  },
+  {
+    "revision": "b164411eb10667c9860a7372e97b2ecb",
+    "url": "/portfolio-react/static/media/astronomy.b164411e.jpg"
+  },
+  {
+    "revision": "6f37d5f14eec853874df0c0e4541b164",
+    "url": "/portfolio-react/static/media/backOne.6f37d5f1.jpg"
+  },
+  {
+    "revision": "a4c5d4365a8e2d226665f27142f36bda",
+    "url": "/portfolio-react/static/media/carolinaAzofeifaChinchilla.a4c5d436.pdf"
+  },
+  {
+    "revision": "3a554e81795b260fca2248d5cf75aa17",
+    "url": "/portfolio-react/static/media/creativio.3a554e81.jpg"
+  },
+  {
+    "revision": "53951b8a0d6d2b18c8361a7e279a70fe",
+    "url": "/portfolio-react/static/media/foto1.53951b8a.gif"
+  },
+  {
+    "revision": "4980c7b81ed8c69344943f0873fd0f26",
+    "url": "/portfolio-react/static/media/foto2.4980c7b8.jpg"
+  },
+  {
+    "revision": "6b663ddbc662878d1866aefea78476c9",
+    "url": "/portfolio-react/static/media/foto3.6b663ddb.jpg"
+  },
+  {
+    "revision": "dd0d7f4d9582cc0a449b880b2751405a",
+    "url": "/portfolio-react/static/media/foto4.dd0d7f4d.jpg"
+  },
+  {
+    "revision": "ca57c56fddd0d95651921cbfd0817d58",
+    "url": "/portfolio-react/static/media/foto5.ca57c56f.jpg"
+  },
+  {
+    "revision": "28d923edd846319930340567158217b7",
+    "url": "/portfolio-react/static/media/foto6.28d923ed.jpg"
+  },
+  {
+    "revision": "0d5b55166a9f59bdf4432bb3da2cbd34",
+    "url": "/portfolio-react/static/media/foto7.0d5b5516.png"
+  },
+  {
+    "revision": "d38180e980a01073693be44ea53f8705",
+    "url": "/portfolio-react/static/media/foto8.d38180e9.jpg"
+  },
+  {
+    "revision": "d3adf02f6b49cc109a2e935634335f18",
+    "url": "/portfolio-react/static/media/header3.d3adf02f.jpg"
+  },
+  {
+    "revision": "7ace99382b4bf45a4c5eb2755e2d74d3",
+    "url": "/portfolio-react/static/media/me.7ace9938.png"
+  },
+  {
+    "revision": "1f654cd78d830b4e87d68c34c0f81f2b",
+    "url": "/portfolio-react/static/media/me5.1f654cd7.png"
+  },
+  {
+    "revision": "7b99454981f0895af07527183253a053",
+    "url": "/portfolio-react/static/media/reactimg.7b994549.png"
+  }
+]);
